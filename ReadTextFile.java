e44cb6fb4beb45f5ea36d4b0d62f97d40e7459b6
Vikram Mahendru <vikrammahendru@Admins-MacBook-Pro.local>
2017-03-27 14:01:26 -0400

    added file reader

---
+++ ReadTextFile.java
@@ -0,0 +1,37 @@
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class ReadTextFile {
+	static ArrayList<String> list = new ArrayList<String>();
+
+	
+	
+    public static void main(String[] args) throws IOException {
+
+        try {
+
+            File f = new File("src/Untitled.txt");
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader(f));
+
+            String readLine = "";
+
+            System.out.println("YAS");
+
+            while ((readLine = bufferedReader.readLine()) != null) {
+                System.out.println(readLine);
+                list.add(readLine);
+                
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
