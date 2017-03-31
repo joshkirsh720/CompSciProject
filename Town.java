20c0bd983c454286e00472c1f2072dd70cd0d5fb
joshkirsh720 <joshua.kirshenbaum@gmail.com>
2017-03-31 14:26:06 -0400

    Started town init

--- Town.java
+++ Town.java
@@ -1,6 +1,16 @@
 
  
 public class Town {
+	private Location[][] town = new Location[500][500];
 	
+	public Town() {
+	}
 	
+	public init() {
+		for(int x = 0; x <500; x++) {
+			for(int y = 0; y < 500; y++) {
+				//ADD LOCATIONS TO TOWN ARRAY HERE
+			}
+		}
+	}
 }
