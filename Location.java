5db4b8b5ce1bb9ffb2df0b3b42227aaf3880b477
joshkirsh720 <joshua.kirshenbaum@gmail.com>
2017-03-31 14:12:48 -0400

    wrote location class

--- Location.java
+++ Location.java
@@ -1,8 +1,31 @@
 
  
 public class Location {
-    
-
+    private int x, y;
 	
+	//0=house
+	//1=Bart Complex
+	//2=Lisa Complex
+	//3=Delivery Center
+	private int locationType;
 	
+	public Location(int xVal, int yVal, int locT) {
+		x = xVal;
+		y = yVal;
+		locationType = locT;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getLocationType() {
+		return locationType;
+	}
+	public int[] getCoordinates() {
+		int[] coord = {x,y};
+		return coord;
+	}
 }
