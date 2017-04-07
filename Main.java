3204c2daef64a0af76798716ba3584cadb39e65d
joshkirsh720 <joshua.kirshenbaum@gmail.com>
2017-04-06 21:51:06 -0400

    created new function to separate location strings

--- Main.java
+++ Main.java
@@ -8,9 +8,11 @@ import java.util.ArrayList;
 
 public class Main {
     
+	static int bartDeliveries, lisaDeliveries, packagesToDeliver;
     
 	public static void main(String[] args) throws IOException {
-        System.out.println(readFile());
+		ArrayList<String> list = readFile();
+		convertStringArrayToLocations(list);
 		//TODO
 	}
     
@@ -18,6 +20,7 @@ public class Main {
     
     public static ArrayList<String> readFile() throws IOException {
         ArrayList<String> list = new ArrayList<String>();
+		
 
         try {
 
@@ -27,10 +30,40 @@ public class Main {
             
             String readLine = "";
             
+			int counter=0;
+			boolean bartGet=false,lisaGet=false;
             while ((readLine = bufferedReader.readLine()) != null) {
                 System.out.println(readLine);
-                list.add(readLine);
+                counter++;
+				if(counter==1) {
+					continue;
+				}
+				else if(counter==2) {
+					packagesToDeliver = Integer.parseInt(readLine);
+					continue;
+				}
                 
+				if(readLine.equals("Bart Complex")) {
+					bartGet=true;
+					continue;
+				}
+				else if(readLine.equals("Lisa Complex")) {
+					lisaGet=true;
+					continue;
+				}
+				
+				if(bartGet==true) {
+					bartDeliveries=Integer.parseInt(readLine);
+					bartGet=false;
+					continue;
+				}
+				else if(lisaGet==true) {
+					lisaDeliveries=Integer.parseInt(readLine);
+					lisaGet=false;
+					continue;
+				}
+				
+				list.add(readLine);
             }
             
         } catch (IOException e) {
@@ -39,5 +72,24 @@ public class Main {
         return list;
     
     }
+	
+	public static void convertStringArrayToLocations(ArrayList<String> list) {
+		ArrayList<String> newList = new ArrayList<String>();
+		
+		for(int i = 0; i < list.size(); i++) {
+			
+			String arrayGet = list.get(i);
+			int startGrab = 0;
+			for(int x = 0; x < arrayGet.length(); x++) {
+				if(arrayGet.charAt(x) == ',') {
+					newList.add(arrayGet.substring(startGrab, x));
+					startGrab = x+1;
+				}
+				else if (x == arrayGet.length()-1) {
+					newList.add(arrayGet.substring(startGrab, x+1));
+				}
+			}
+		}
+	}
     
 }
