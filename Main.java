95d1d2b898e618d73784987242aa015eca275a40
joshkirsh720 <joshua.kirshenbaum@gmail.com>
2017-04-06 22:52:22 -0400

    finished creating final list of delivery locations

--- Main.java
+++ Main.java
@@ -9,10 +9,12 @@ import java.util.ArrayList;
 public class Main {
     
 	static int bartDeliveries, lisaDeliveries, packagesToDeliver;
+	static Location bartComplexLocation = new Location(3,29,1);
+	static Location lisaComplexLocation = new Location(297,329,2);
     
 	public static void main(String[] args) throws IOException {
 		ArrayList<String> list = readFile();
-		convertStringArrayToLocations(list);
+		ArrayList<Location> deliveryLocationList = convertStringArrayToLocations(list);
 		//TODO
 	}
     
@@ -73,7 +75,7 @@ public class Main {
     
     }
 	
-	public static void convertStringArrayToLocations(ArrayList<String> list) {
+	public static ArrayList<Location> convertStringArrayToLocations(ArrayList<String> list) {
 		ArrayList<String> separatedList = new ArrayList<String>();
 		ArrayList<String> cleanedList = new ArrayList<String>();
 		ArrayList<Location> finalList = new ArrayList<Location>();
@@ -94,7 +96,7 @@ public class Main {
 				}
 			}
 		}
-		System.out.println(separatedList);
+		System.out.println(1 + "" + separatedList);
 		
 		//removes all unecessary letters like the s and a at the end of the first 2 elements in each location
 		for(int i = 0; i < separatedList.size(); i++) {
@@ -113,9 +115,59 @@ public class Main {
 			}
 			cleanedList.add(result);
 		}
-		System.out.println(cleanedList);
+		System.out.println(2 + "" + cleanedList);
 		
 		//creates locations based off of what is now in the cleaned list and adds them to the final list
+		int countTo = packagesToDeliver - (bartDeliveries+lisaDeliveries);
+		int beginning = 0;
+		for(int i = 1; i <= countTo; i++) {
+			ArrayList<String> houseLocation = new ArrayList<String>();
+			
+			int end = (3 * i);
+			for(int x = beginning; x < end; x++) {
+				houseLocation.add(cleanedList.get(x));
+			}
+			beginning = end;
+			System.out.println(3 + "" + houseLocation);
+			
+			finalList.add(determineLocation(houseLocation));
+		}
+		
+		if(bartDeliveries != 0) {
+			finalList.add(bartComplexLocation);
+		}
+		if(lisaDeliveries != 0) {
+			finalList.add(lisaComplexLocation);
+		}
+		
+		return finalList;
 	}
     
+	public static Location determineLocation(ArrayList<String> list) {
+		Location finalLocation;
+		int street = Integer.parseInt(list.get(0));
+		int avenue = Integer.parseInt(list.get(1));
+		String houseLetter = list.get(2);
+		char houseLetterAsChar = houseLetter.charAt(0);
+		
+		int x, y;
+		
+		//using street to get x
+		x = 2 * (street-1);
+		//figuring out what side of the street the house is on and adding one if it is on the right side.
+		if(houseLetter.length() == 2) {
+			x++;
+		}
+		System.out.println(3.5 + " " + x);
+		
+		//using avenue to get y
+		y = 10 * (avenue -1);
+		System.out.println(4 + " " + y);
+		//using ASCII value of the letter to find where the house is vertically in between avenues.
+		y += (int)houseLetterAsChar - 65;
+		System.out.println(5 + " " + y);
+		
+		finalLocation = new Location(x,y,0);
+		return finalLocation;
+	}
 }
