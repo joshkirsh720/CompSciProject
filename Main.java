c53c5004e4e93e48744572e12a39d7650d7c7036
joshkirsh720 <joshua.kirshenbaum@gmail.com>
2017-04-06 22:08:19 -0400

    wrote code to clean arrayList of unecessary s's and a's

--- Main.java
+++ Main.java
@@ -74,22 +74,48 @@ public class Main {
     }
 	
 	public static void convertStringArrayToLocations(ArrayList<String> list) {
-		ArrayList<String> newList = new ArrayList<String>();
+		ArrayList<String> separatedList = new ArrayList<String>();
+		ArrayList<String> cleanedList = new ArrayList<String>();
+		ArrayList<Location> finalList = new ArrayList<Location>();
 		
+		//separates original list elements into their own spot in the array
+		//ex. "1s,44a,EE" gets separated to "1s","44a","EE"
 		for(int i = 0; i < list.size(); i++) {
 			
 			String arrayGet = list.get(i);
 			int startGrab = 0;
 			for(int x = 0; x < arrayGet.length(); x++) {
 				if(arrayGet.charAt(x) == ',') {
-					newList.add(arrayGet.substring(startGrab, x));
+					separatedList.add(arrayGet.substring(startGrab, x));
 					startGrab = x+1;
 				}
 				else if (x == arrayGet.length()-1) {
-					newList.add(arrayGet.substring(startGrab, x+1));
+					separatedList.add(arrayGet.substring(startGrab, x+1));
 				}
 			}
 		}
+		System.out.println(separatedList);
+		
+		//removes all unecessary letters like the s and a at the end of the first 2 elements in each location
+		for(int i = 0; i < separatedList.size(); i++) {
+			
+			String arrayGet = separatedList.get(i);
+			StringBuilder sb = new StringBuilder(arrayGet);
+			String result = "";
+			for(int x = 0; x < arrayGet.length(); x++) {
+				if(arrayGet.charAt(x) == 's' || arrayGet.charAt(x) == 'a') {
+					sb.deleteCharAt(x);
+					result = sb.toString();
+				}
+				else {
+					result = arrayGet;
+				}
+			}
+			cleanedList.add(result);
+		}
+		System.out.println(cleanedList);
+		
+		//creates locations based off of what is now in the cleaned list and adds them to the final list
 	}
     
 }
